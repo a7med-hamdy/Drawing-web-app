7393ba2950531c40d878cf728899736e4837673a
a7med-hamdy <hamdiahmed906@gmail.com>
2021-12-03 14:06:50 +0200

    start

---
+++ back-end/drawing-server/src/main/java/com/example/drawingserver/DrawingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.drawingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawingServerApplication.class, args);
+	}
+
+}
---
+++ back-end/drawing-server/src/test/java/com/example/drawingserver/DrawingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drawingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
