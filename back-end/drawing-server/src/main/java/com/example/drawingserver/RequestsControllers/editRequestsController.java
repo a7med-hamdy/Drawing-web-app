cc67ce9f5147e88e995e7c895f0383cfb517f2b7
a7med-hamdy <hamdiahmed906@gmail.com>
2021-12-06 19:38:39 +0200

    added json

--- back-end/drawing-server/src/main/java/com/example/drawingserver/RequestsControllers/editRequestsController.java
+++ back-end/drawing-server/src/main/java/com/example/drawingserver/RequestsControllers/editRequestsController.java
@@ -1,9 +1,13 @@
 package com.example.drawingserver.RequestsControllers;
 
 
+import java.util.EmptyStackException;
+
 import com.example.drawingserver.shapes.shapeFactroy;
 import com.example.drawingserver.shapes.shapeInterface;
 import com.example.drawingserver.shapes.shapeWarehouse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,19 +23,20 @@ public class editRequestsController{
 
     shapeFactroy factory;
     shapeWarehouse warehouse;
+    ObjectMapper map;
 
     public editRequestsController(){
         this.warehouse = shapeWarehouse.getInstanceOf();
         this.factory = new shapeFactroy();
+        this.map = new ObjectMapper();
     }
     // clicking on a shape from the provided shapes to create it
     // (waiting for clicking on the board to insert the object)
     @PostMapping("/create:{shape}")
-    public String[] createShape(@PathVariable String shape){
+    public String createShape(@PathVariable String shape) throws JsonProcessingException{
         shapeInterface s = this.factory.factorShape(shape);
         this.warehouse.addShape(s);
-        System.out.println(s.toString());
-        return s.toStrings();
+        return map.writeValueAsString(s);
     }
 
 
@@ -46,35 +51,79 @@ public class editRequestsController{
     // changing the color of the shape
     @PostMapping("/{id}/color:{color}")
     public String setColor(@PathVariable int id, @PathVariable String color){
-        this.warehouse.changeColor(id, color);
+        try{
+            this.warehouse.changeColor(id, color);
+        }
+        catch(IndexOutOfBoundsException e){
+            return "fail";
+        }
         return "success";
     }
 
     // resizing the shape
     @PostMapping("/{id}/resize:{v1},{v2}")
     public String resizeReq(@PathVariable int id, @PathVariable int v1, @PathVariable int v2){
-        this.warehouse.changeSize(id, v1, v2);;
+        try{
+            this.warehouse.changeSize(id, v1, v2);
+        }
+        catch(IndexOutOfBoundsException e){
+            return "fail";
+        }
         return "success";
     }
 
     // moving the shape
     @PostMapping("/{id}/move:{x},{y}")
-    public String moveReq(@PathVariable int id, @PathVariable int x, @PathVariable int y){
-        this.warehouse.changeLocation(id, x, y);;
+    public String moveReq(@PathVariable int id, @PathVariable int x, @PathVariable int y){   
+        try{
+            this.warehouse.changeLocation(id, x, y);
+        }
+        catch(IndexOutOfBoundsException e){
+            return "fail";
+        }
         return "success";
     }
 
     // copying the shape will create an object waiting to be pasted
     @PostMapping("/{id}/copy")
-    public String[] copyReq(@PathVariable int id) throws CloneNotSupportedException{
+    public String copyReq(@PathVariable int id) throws CloneNotSupportedException, JsonProcessingException{
         shapeInterface s = this.warehouse.copyShape(id);
-        return s.toStrings();
+        return map.writeValueAsString(s);
     }
 
     // delete the shape from the shapes list
     @PostMapping("/{id}/delete")
-    public String deleteReq(@PathVariable int id){
-        this.warehouse.removeShape(id);
+    public String deleteReq(@PathVariable int id){   
+        try{
+            this.warehouse.removeShape(id);
+        }
+        catch(IndexOutOfBoundsException e){
+            return "fail";
+        }
+        return "success";
+    }
+
+    @PostMapping("/undo")
+    public String undoReq()
+    {
+        try{
+            this.warehouse.undo();
+        }
+        catch(EmptyStackException e){
+            return "fail";
+        }
+        return "success";
+    }
+
+    @PostMapping("/redo")
+    public String redoReq()
+    {
+        try{
+            this.warehouse.redo();
+        }
+        catch(EmptyStackException e){
+            return "fail";
+        }
         return "success";
     }
 
--- back-end/drawing-server/src/main/java/com/example/drawingserver/shapes/shapeWarehouse.java
+++ back-end/drawing-server/src/main/java/com/example/drawingserver/shapes/shapeWarehouse.java
@@ -1,11 +1,12 @@
 package com.example.drawingserver.shapes;
 import java.util.Stack;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 public class shapeWarehouse {
-    private Stack<ArrayList<shapeInterface>> undo = new Stack<ArrayList<shapeInterface>>();
-    private Stack<ArrayList<shapeInterface>> redo = new Stack<ArrayList<shapeInterface>>();
-    private ArrayList<shapeInterface> shapes = new ArrayList<shapeInterface>();
+    private Stack<ArrayList<shapeInterface>> undo = new Stack<>();
+    private Stack<ArrayList<shapeInterface>> redo = new Stack<>();
+    private ArrayList<shapeInterface> shapes = new ArrayList<>();
     private static shapeWarehouse instance;
     private shapeWarehouse(){}
 
@@ -24,7 +25,7 @@ public class shapeWarehouse {
         this.redo.clear();
     }
 
-    public void removeShape(int id)
+    public void removeShape(int id)throws ArrayIndexOutOfBoundsException
     {
         int index = getShapeIndex(id);
         this.shapes.remove(index);
@@ -36,24 +37,25 @@ public class shapeWarehouse {
     {
         int index = getShapeIndex(id);
         shapeInterface s = this.shapes.get(index).clone();
+        this.shapes.add(s);
         this.undo.push(this.shapes);
         this.redo.clear();
         return s;
     }
 
-    public ArrayList<shapeInterface> undo(){
-        ArrayList<shapeInterface> temp = this.undo.pop();
-        this.redo.push(temp);
-        return temp;
+    public ArrayList<shapeInterface> undo() throws EmptyStackException{
+        this.shapes = this.undo.pop();
+        this.redo.push(this.shapes);
+        return this.shapes;
     }
 
-    public ArrayList<shapeInterface> redo(){
-        ArrayList<shapeInterface> temp = this.redo.pop();
-        this.undo.push(temp);
-        return temp;
+    public ArrayList<shapeInterface> redo() throws EmptyStackException{
+        this.shapes = this.redo.pop();
+        this.undo.push(this.shapes);
+        return this.shapes;
     }
 
-    public void changeColor(int id, String color)
+    public void changeColor(int id, String color)throws ArrayIndexOutOfBoundsException
     {
         int index = getShapeIndex(id);
         this.shapes.get(index).setColor(color);
@@ -61,7 +63,7 @@ public class shapeWarehouse {
         this.redo.clear();
     }
 
-    public void changeSize(int id, int x, int y)
+    public void changeSize(int id, int x, int y)throws ArrayIndexOutOfBoundsException
     {
         int index = getShapeIndex(id);
         this.shapes.get(index).setDimensions(x, y);
@@ -69,7 +71,7 @@ public class shapeWarehouse {
         this.redo.clear();
     }
 
-    public void changeLocation(int id, int x, int y)
+    public void changeLocation(int id, int x, int y)throws ArrayIndexOutOfBoundsException
     {
         int index = getShapeIndex(id);
         this.shapes.get(index).setPostion(x, y);
@@ -79,10 +81,10 @@ public class shapeWarehouse {
 
     private int getShapeIndex(int id)
     {
-        int i;
+        int i = -1;
         for (i = 0; i < this.shapes.size(); i++)
         {
-            if(this.shapes.get(i).idgetter() == id)
+            if(this.shapes.get(i).getID() == id)
             {
                 break;
             }
