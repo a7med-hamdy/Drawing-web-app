911b2d615df07363bbbb39272e2ab284a09c2719
Ahmed-Ashour-2024 <mody.ashour.726@gmail.com>
2021-12-06 05:18:46 +0200

    add HttpClientModule

--- front-end/drawing-app/src/app/app.module.ts
+++ front-end/drawing-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { KonvaModule } from 'ng2-konva';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
@@ -12,7 +13,8 @@ import { CanvasComponent } from './canvas/canvas.component';
   ],
   imports: [
     BrowserModule,
-    KonvaModule
+    KonvaModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
