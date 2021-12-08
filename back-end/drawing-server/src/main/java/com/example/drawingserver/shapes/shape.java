056f55b64e18b9ecc4cc9d1b11663128f224410b
aaserfawzy <aaserkind537@gmail.com>
2021-12-08 16:48:13 +0200

    copy()

--- back-end/drawing-server/src/main/java/com/example/drawingserver/shapes/shape.java
+++ back-end/drawing-server/src/main/java/com/example/drawingserver/shapes/shape.java
@@ -14,25 +14,30 @@ class shape implements Cloneable,shapeInterface{
     protected String stroke;
     protected int strokeWidth;
 
+    @Override
     public void setPostion( int x, int y){
         this.x=x;
         this.y=y;
     }
 
+    @Override
     public void setDimensions(int dimention1,int dimention2){
         this.dimension1=dimention1;
         this.dimension2=dimention2;
     }
 
+    @Override
     public void setDimensions(int dimention){
         this.dimension1=dimention;
         this.dimension2=dimention;
     }
 
+    @Override
     public  void setColor(String fill){
         this.fill=fill;
     }
 
+    @Override
     public void setStrock(String stroke){
         this.stroke=stroke;
     }
@@ -40,32 +45,40 @@ class shape implements Cloneable,shapeInterface{
     public void setstrockWidth(int strokeWidth){
         this.strokeWidth=strokeWidth;
     }
-    
+
+    @Override
     public int[] getPostion(){
         int[] x={this.x,this.y};
         return x;
     }
 
+    @Override
     public int[] getvalues(){
         int[] x={this.dimension1,this.dimension2};
         return x;
     }
+
+    @Override
    public String getStrokeColor(){
         return this.stroke;
    }
 
+   @Override
     public int getstrokeWidth(){
         return this.strokeWidth;
     }
 
+    @Override
     public int getID(){
         return id;
     }
 
+    @Override
     public String getType(){
         return type;
     }
 
+    @Override
     public String getColor(){
         return this.fill;
     }
@@ -80,6 +93,22 @@ class shape implements Cloneable,shapeInterface{
 
         a.type=this.type;
         a.id=rand;
+        a.x=this.x;
+        a.y=this.y;
+        a.dimension1=this.dimension1;
+        a.dimension2=this.dimension2;
+        a.fill=this.fill;
+        a.stroke=this.stroke;
+        a.strokeWidth=this.strokeWidth;
+
+        return a;
+    }
+
+    public shapeInterface copy(){
+        shape a=new shape();
+
+        a.type=this.type;
+        a.id=this.id;
         a.x=this.x;
         a.y=this.y;
         a.dimension1=this.dimension1;
--- back-end/drawing-server/src/main/java/com/example/drawingserver/shapes/shapeInterface.java
+++ back-end/drawing-server/src/main/java/com/example/drawingserver/shapes/shapeInterface.java
@@ -30,4 +30,5 @@ public interface shapeInterface {
     String getType();
 
     shapeInterface clone() throws CloneNotSupportedException;
+    
 }
