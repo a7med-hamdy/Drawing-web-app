afa700347be8a16d25892c8834b0af4c984877c5
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2021-12-10 20:05:53 +0200

    comments and removed redundant variables and attributes

--- front-end/drawing-app/src/app/canvas/canvas-manager.service.ts
+++ front-end/drawing-app/src/app/canvas/canvas-manager.service.ts
@@ -9,13 +9,12 @@ import Konva from 'konva';
   providedIn: 'root'
 })
 export class CanvasManagerService {
-  pressed:boolean = false;
-  shapes: any = [];
-  stage!: Konva.Stage;
-  layer!: Konva.Layer;
-  Cursor!:CursorService;
-  ShapeTranslator:ShapeTranslatorService = new ShapeTranslatorService();
-  shape!: any;
+
+  shapes: any = [];//array of shapes currently present in the layer
+  stage!: Konva.Stage;//konva stage that hold the layer
+  layer!: Konva.Layer;//Konva layer that holds the shapes
+  Cursor!:CursorService; //cursor used to select shapes on the canvas
+  ShapeTranslator:ShapeTranslatorService = new ShapeTranslatorService(); //builds Konva shapes from backend shapes
   selectedFiles?: File;
 
   constructor(stg:Konva.Stage, lyer:Konva.Layer, public req:RequestsService) {
@@ -24,19 +23,35 @@ export class CanvasManagerService {
   }
 
 /*Utility Functions */
+
+/**
+ *
+ * @param shape shape to be added
+ *
+ * Adds the given shape to the canvas & shapes array
+ *
+ */
   public addShape(shape:any){
+    //push the shape in the array and onto the layer
     this.layer.add(shape);
     this.shapes.push(shape);
-    console.log(this.layer.getChildren());
   }
 
+/**
+ *
+ * Requests & loads data from the drawing server when the application is launched
+ * or opened in another tab
+ */
   public requestData():void{
+    //request the shapes array from the backend
     this.req.refreshRequest()
     .subscribe(data =>{
       console.log(data);
+      //if the array is empty return
       if (data.length === 0 ){
         return;
       }
+      //if not add the shapes on the canvas
       else{
         for(var i = 0; i < data.length; i++){
             var newShape = this.ShapeTranslator.translateToKonva(data[i]);
@@ -47,30 +62,50 @@ export class CanvasManagerService {
     })
   }
 
+  /**
+   *
+   * function to keep refreshing the CanvasManager and its cursor
+   *  on start of the application or when a change is detected
+   *
+   * this function is put in ngOnIt of the canvas component
+   *
+   */
    public refresh():void{
+     //create the stage on start
     this.stage = new Konva.Stage({
       container: 'container',
       width: window.innerWidth,
       height: window.innerHeight,
     });
-    this.Cursor = new CursorService(this.stage, this.req);
-    this.layer = new Konva.Layer();
-    this.layer.add(this.Cursor.transformer);
-    this.stage.add(this.layer);
-    this.Cursor.CursorPositionListener();
-    this.Cursor.CursorShapeSelectionListener(this.shapes, this.layer);
-    this.Cursor.CursorTransformationListener();
-    this.Cursor.CursorDraggerListener();
-    this.requestData();
+    this.Cursor = new CursorService(this.stage, this.req); //create the cursor on start
+    this.layer = new Konva.Layer();  //create layer on start
+    this.layer.add(this.Cursor.transformer); //add the cursor's transformer to the layer on start
+    this.stage.add(this.layer); //add the layer to the stage on start
+    this.Cursor.CursorPositionListener(); //listen for the cursor's position
+    this.Cursor.CursorShapeSelectionListener(this.layer); //listen for the selection events
+    this.Cursor.CursorTransformationListener();//listen for the transformation events
+    this.Cursor.CursorDraggerListener();//listen for the dragging events
+    this.requestData(); // request the data on start
 
   }
 ///////////////////////////////////////////////////////////////////////
+/*Main functions */
 
-
+/**
+ *
+ * @param type type of shape to be created
+ * sends a create request in a desired location
+ *  recieves the desired shape and adds it to the canvas.
+ *
+ *
+ */
   createShape(type: string){
     const component = this;
     var x, y;
+
+    //when clicking on the stage
     this.stage.on('click', function(){
+      //send a create request with the current cursor position
         x = component.Cursor.CursorPos.x;
         y = component.Cursor.CursorPos.y;
         component.req.createRequest(type, Math.trunc(x), Math.trunc(y))
@@ -78,17 +113,28 @@ export class CanvasManagerService {
             component.addShape(component.ShapeTranslator.translateToKonva(data));
             console.log(`${type} is created\n` + JSON.stringify(data))
           });
+          //stop listening for click events to avoid creating multiple shapes on every click
           component.stage.off('click');
     return type;
     });
   }
 
 
+/**
+ *
+ * sends a copy request for the id of shape that is selected by the cursor
+ * and recieving the shape and adding it at the desired location.
+ */
   public copyShape(){
+    //if there is a selection
+    if(this.Cursor.selectedShape != null){
     const component = this;
-    var id = this.Cursor.selectedShape.getAttr('id');
+    var id = this.Cursor.selectedShape.getAttr('id'); //id of the selected shape
     var x,y;
+
+    //when clicking on the stage
     this.stage.on('click', function(){
+        //send a copy request with the current cursor position
         x = component.Cursor.CursorPos.x;
         y = component.Cursor.CursorPos.y;
         component.req.copyRequest(id, Math.trunc(x), Math.trunc(y))
@@ -96,16 +142,24 @@ export class CanvasManagerService {
             component.addShape(component.ShapeTranslator.translateToKonva(data));
             console.log(`shape is copied\n` + JSON.stringify(data));
         });
+        //stop listening for click events to avoid creating multiple shapes on every click
         component.stage.off('click');
     });
+  }
   }
 
 
 
+/**
+ * sends a delete request for the id of shape that is selected by the cursor
+ * deletes a shape selected by the cursor from the canvas.
+ *
+ */
   public deleteShape(){
+    //if there is a selection
     if(this.Cursor.selectedShape != null){
-      var id = this.Cursor.selectedShape.getAttr('id');
-
+      var id = this.Cursor.selectedShape.getAttr('id');//id of the selected shape
+      //search for the id of the shape in the array and remove it
       for( var i = 0; i < this.shapes.length; i++){
         if ( this.shapes[i].getAttr('id') === id) {
             this.Cursor.selectedShape.destroy();
@@ -113,6 +167,7 @@ export class CanvasManagerService {
             break;
         }
       }
+      //send the delete request with the id of the shape
       this.req.deleteRequest(id)
       .subscribe(data => {
         this.Cursor.emptySelection();
@@ -122,11 +177,22 @@ export class CanvasManagerService {
     }
 }
 
-  public colorShape(color:string){
 
+/**
+ *
+ * @param color the desired color
+ *
+ *
+ * Send a color request for the id of the shape selected by the cursor
+ * colors the shape selected by the cursor
+ *
+ */
+  public colorShape(color:string){
+    //if there is a selection
     if(this.Cursor.selectedShape != null){
-      var id = this.Cursor.selectedShape.getAttr('id');
+      var id = this.Cursor.selectedShape.getAttr('id');//id of the selected shape
       console.log(this.Cursor.selectedShape);
+      //change its color
       this.Cursor.selectedShape.setAttrs({
         fill:color
       });
@@ -136,22 +202,33 @@ export class CanvasManagerService {
           stroke:color,
         })
       }
+      //send the color request to the backend
       this.req.colorRequest(id, color)
       .subscribe(data => { console.log(`color changed to: ${color}\nshape #${id}\n` + data) });
     }
   }
 
 
+  /**
+   * sends an undo request to the backend,
+   * recieves the new shapes array to be built
+   * builds the new array on the canvas.
+   *
+   */
   public undo(){
+    //empty all selections if there is any
     this.Cursor.emptySelection();
+    //send the undo request
     this.req.undoRequest()
     .subscribe(data =>{
+        //remove all data from layer and shapes and build the new canvas from the recieved data
           this.layer.removeChildren();
           this.shapes = [];
           for(var i = 0; i < data.length; i++){
               var newShape = this.ShapeTranslator.translateToKonva(data[i]);
               this.addShape(newShape);
           }
+          //add the transformer back
           this.layer.add(this.Cursor.transformer);
           console.log("return: \n"+data);
           console.log(`UNDO action:\n` + JSON.stringify(data))
@@ -159,42 +236,67 @@ export class CanvasManagerService {
   }
 
 
+   /**
+   * sends an redo request to the backend,
+   * recieves the new shapes array to be built
+   * builds the new array on the canvas.
+   *
+   */
   public redo(){
+    //empty all selections if there is any
     this.Cursor.emptySelection();
     this.req.redoRequest()
+    //send the redo request
      .subscribe(data =>{
+       //remove all data from layer and shapes and build the new canvas from the recieved data
           this.layer.removeChildren();
           this.shapes = [];
           for(var i = 0; i < data.length; i++){
               var newShape = this.ShapeTranslator.translateToKonva(data[i]);
               this.addShape(newShape);
           }
+          //add the transformer back
           this.layer.add(this.Cursor.transformer);
         console.log(`REDO action:\n` + JSON.stringify(data))
       });
   }
 
-public Uploadfile(event: any):void{
-  this.selectedFiles = event.target.files[0];
-  const reader = new FileReader();
-  reader.onloadend = (e) => {
-    let x = reader.result as string;
-    this.req.upload(x, <string>this.selectedFiles?.type)
-    .subscribe(data =>
-      {
-        console.log(data[0])
-        if(data.length == 0){return;}
-        this.layer.removeChildren();
-        this.shapes = [];
-        for(var i = 0; i < data.length; i++){
-          var newShape = this.ShapeTranslator.translateToKonva(data[i]);
-          this.addShape(newShape);
-        }
-        if(this.Cursor.selectedShape != null)
-          this.Cursor.emptySelection();
-        this.layer.add(this.Cursor.transformer);
-      });
- };
-  reader.readAsText(event.target.files[0])
+
+  /**
+   *
+   * @param event uploaded file
+   *
+   * sends the uploaded file to the backend to be loaded
+   * recieves the shapes array to be built on the canvas
+   * builds the shapes on the canvas.
+   */
+  public Uploadfile(event: any):void{
+    //read the uploaded file
+    this.selectedFiles = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = (e) => {
+      let x = reader.result as string;
+      //send the file to the back end to be processed
+      this.req.upload(x, <string>this.selectedFiles?.type)
+      //recieve the shapes to be built
+      .subscribe(data =>
+        {
+          console.log(data[0])
+          if(data.length == 0){return;}
+          //remove all data from layer and shapes and build the new canvas from the recieved data
+          this.layer.removeChildren();
+          this.shapes = [];
+          for(var i = 0; i < data.length; i++){
+            var newShape = this.ShapeTranslator.translateToKonva(data[i]);
+            this.addShape(newShape);
+          }
+          //empty all selections
+          if(this.Cursor.selectedShape != null)
+            this.Cursor.emptySelection();
+            //add back the transformer to the layer
+          this.layer.add(this.Cursor.transformer);
+        });
+    };
+    reader.readAsText(event.target.files[0])
 }
 }
--- front-end/drawing-app/src/app/canvas/cursor.service.ts
+++ front-end/drawing-app/src/app/canvas/cursor.service.ts
@@ -5,22 +5,28 @@ import Konva from 'konva';
   providedIn: 'root'
 })
 export class CursorService {
-  CursorPos:any;
-  stage!: Konva.Stage;
+  CursorPos:any; //the position of the cursor on the stage
+  stage!: Konva.Stage; //the stage on which events are listened to and updated accordingly
+  selectedShape!:any; //a pointer to the selected shape on the canvas
 
-  selectedShape!:any;
-
+  //Konva transformer used to resize Konva shapes
   transformer: Konva.Transformer = new Konva.Transformer({
     rotateEnabled:false,
     ignoreStroke:true
   });
-  lineAnchor1!:any;
-  lineAnchor2!:any;
+
+  //special transformers for the line using two Konva circles
+  lineAnchor1!:any; //updates the X & Y coordinates
+  lineAnchor2!:any; //updates the position coordinates
 
   constructor(stg:Konva.Stage, public req: RequestsService) {
     this.stage = stg;
   }
 
+  /**
+   * resets all values of the pointers and
+   * detaches all the transformers
+   */
   public emptySelection(){
     this.selectedShape = null;
     this.transformer.setAttrs({
@@ -47,6 +53,16 @@ export class CursorService {
     this.transformer.nodes([]);
   }
 
+  /**
+   *
+   * @param targetX new X location
+   * @param targetY new Y location
+   * @param targetWdth new dimension 1
+   * @param targetHeigt new dimension 2
+   * @param id id of the shape
+   *
+   * sends a resize request for the shape currently transforming.
+   */
   public sendResize(targetX:any, targetY:any,targetWdth:any, targetHeigt:any, id:any){
     this.req.resizeRequest(id, Math.trunc(targetWdth), Math.trunc(targetHeigt),Math.trunc(targetX),Math.trunc(targetY))
     .subscribe(data => {
@@ -54,13 +70,25 @@ export class CursorService {
       console.log(`size changed to: (${targetWdth},${targetHeigt})\nshape #${id}\n` + data)
     });
   }
-
+/**
+ *
+ * @param targetX new X location
+ * @param targetY new Y location
+ * @param id id of the shape
+ *
+ * sends a move request for the shape currently being dragged.
+ */
   public sendMove(targetX:any, targetY:any, id:any){
     this.req.moveRequest(id, Math.trunc(targetX), Math.trunc(targetY))
     .subscribe(data => {
       console.log(`position changed to: (${targetX},${targetY})\nshape #${id}\n` + data)
     })
   }
+
+  /**
+   * listens on the position of the cursor on the stage
+   * updates the cursor coordinates accordingly
+   */
   public CursorPositionListener(){
     const component = this;
      this.stage.on('mousemove', function(){
@@ -68,8 +96,15 @@ export class CursorService {
     });
   }
 
-
-  public CursorShapeSelectionListener(shapes:any, lyer:Konva.Layer){
+  /**
+   *
+   * @param lyer konva layer to add the line anchors on
+   *
+   * listens on the double click for the cursor selections
+   *
+   * attaches/detaches the transformer accordingly
+   */
+  public CursorShapeSelectionListener(lyer:Konva.Layer){
     const component = this;
     //selection on click
     this.stage.on('dblclick', function (e) {
@@ -78,12 +113,15 @@ export class CursorService {
         component.emptySelection();
         return;
       }
+      //if selecting line anchors - do nothing
       if(e.target === component.lineAnchor1 || e.target === component.lineAnchor2){
         return;
       }
+
+      //if the selected target is a shape
       if(e.target instanceof Konva.Shape){
 
-        //filter the selected shapes if multiple clicks on the same shape
+        //attach corner anchors only for the circle, triangle and square
         if(e.target instanceof Konva.Circle || e.target.name() == 'square' || e.target.name() == 'triangle'){
           component.emptySelection();
           component.transformer.setAttrs({
@@ -97,6 +135,8 @@ export class CursorService {
           component.selectedShape = e.target;
           component.transformer.nodes([component.selectedShape]);
         }
+
+        //attach the special line anchors for the line
         else if(e.target instanceof Konva.Line){
           component.emptySelection();
           component.selectedShape = e.target;
@@ -130,27 +170,14 @@ export class CursorService {
             e.target.setAttrs({
               points:pointsArr,
             });
-            //lyer.batchDraw();
           }
-
           component.lineAnchor1.on('dragmove', updateLine);
           component.lineAnchor2.on('dragmove', updateLine);
 
         }
+        //other wise attach the full transformer
         else{
           component.emptySelection();
-          component.transformer.setAttrs({
-            enabledAnchors:[
-              'top-left',
-              'top-center',
-              'top-right',
-              'middle-right',
-              'middle-left',
-              'bottom-left',
-              'bottom-center',
-              'bottom-right'
-            ]
-          });
           component.selectedShape = e.target;
           component.transformer.nodes([component.selectedShape]);
 
@@ -161,20 +188,33 @@ export class CursorService {
   });
 }
 
+  /**
+   *
+   * @returns if nothing to be listened on
+   *
+   * listen on the transformations done by the transformer
+   * updates the shapes accordingly
+   * sends a resize request to the backend
+   */
   public CursorTransformationListener(){
       const component = this;
       var id;
+      //arrays to filter the noise from the events
       var changesdim:any = [];
       var changespos:any = [];
       this.transformer.on("transformstart", function(e){
         console.log('hello');
+        //if the target is the line anchors return
         if(e.target === component.lineAnchor1 || e.target === component.lineAnchor2){return;}
+
+        //if the target is a shape
         if(e.target instanceof Konva.Shape){
           component.selectedShape = e.target;
           console.log(component.selectedShape);
           e.target.on('transformend', function(){
             id = e.target.getAttr('id');
             var olddate = Date.now();
+            //if the target is a triangle or a circle update its radius attribute with the new changes
             if (e.target instanceof Konva.RegularPolygon || e.target instanceof Konva.Circle){
               e.target.setAttrs({
                 radius:Math.trunc(e.target.radius() * e.target.scaleX()),
@@ -183,6 +223,7 @@ export class CursorService {
               });
               changesdim.push([e.target.radius(),e.target.radius()]);
             }
+            //if the target is an ellipse update both its radiuses attributes with the new changes
             else if(e.target instanceof Konva.Ellipse){
               e.target.setAttrs({
                 radiusX:Math.trunc(e.target.radiusX() * e.target.scaleX()),
@@ -192,6 +233,7 @@ export class CursorService {
               });
               changesdim.push([e.target.radiusX(),e.target.radiusY()]);
             }
+            //if its a rectangle or square update its width and height accordingly
             else{
             e.target.setAttrs({
               width:  Math.trunc(e.target.width() * e.target.scaleX()),
@@ -202,9 +244,11 @@ export class CursorService {
             changesdim.push([e.target.width(),e.target.height()]);
           }
             changespos.push([e.target.x(), e.target.y()]);
+            //filter the noise as much as possible and send the last change made to the backend
             if(Math.abs(Date.now()- olddate) != 0){
               component.sendResize(changespos[changespos.length-1][0],changespos[changespos.length-1][1],changesdim[changesdim.length-1][0],changesdim[changesdim.length-1][1], id);
             }
+            //empty the arrays to avoid overflow errors
             changesdim = [];
             changespos = [];
           });
@@ -213,21 +257,34 @@ export class CursorService {
     return;
   }
 
+  /**
+   *
+   * @returns if nothing to is being dragged
+   *
+   * listens on the dragging events done on the stage
+   * updates the shapes positions accordingly
+   */
   public CursorDraggerListener(){
     const component = this;
-
+    //if dragging happens
     this.stage.on("dragstart", function(e){
+      //if we are selecting a line then the dragged objects are the line anchors
       if(component.selectedShape instanceof Konva.Line){
+        //update the line's position and size according to the anchor's position
         e.target.on('dragend', function(){
           console.log(component.selectedShape);
           component.sendResize(component.lineAnchor1.x(), component.lineAnchor1.y(),component.lineAnchor2.x(), component.lineAnchor2.y(),  component.selectedShape.getAttr('id'));
           console.log(component.selectedShape);
-          e.target.off('dragend')
+          //stop listening for drags to avoid sending multiple requests for the same shape
+          e.target.off('dragend');
         });
     }
+    //if the dragged object is a shape
     else if(e.target instanceof Konva.Shape){
       e.target.on('dragend', function(){
+        //update its X and Y coordinates in the backend
         component.sendMove(Math.trunc(e.target.x()), Math.trunc(e.target.y()), e.target.getAttr('id'));
+        //stop listening for drags to avoid sending multiple requests for the same shape
         e.target.off('dragend');
       });
     }
